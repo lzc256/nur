88370c390becd39d2c091c5ccd3d7e82636d91d9
lzc256 <36326574+lzc256@users.noreply.github.com>
2024-09-07 13:04:21 +0800

    Update default.nix

--- pkgs/cursor-neuro-sama/default.nix
+++ pkgs/cursor-neuro-sama/default.nix
@@ -2,24 +2,15 @@
 , fetchzip
 , stdenv
 }:
-
-let
-  src = url: hash: fetchzip ({
-    url = "https://github.com/ful1e5/apple_cursor/releases/download/v${version}/${variant}.${suffix}";
-    hash = hash;
-  } // (lib.optionalAttrs (suffix == "zip") { stripRoot = false; }));
-
-  version = "0";
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "cursor-neuro-sama";
   version = "0.0";
-  src = fetchFromGitHub 
-
+  src =  
   sourceRoot = ".";
 
   installPhase = ''
-    install -dm 0755 $out/share/icons
-    cp -r macOS* $out/share/icons/
+    mkdir -p $out/share/icons
+    cp -r . $out/share/icons/cursor-neuro-sama
   '';
 
   meta = with lib; {
