ad3776d5a2d1d7c27540cf4c4b085d27406c1601
lzc256 <36326574+lzc256@users.noreply.github.com>
2024-09-07 18:04:29 +0800

    Update default.nix

--- pkgs/cursor-neuro-sama/default.nix
+++ pkgs/cursor-neuro-sama/default.nix
@@ -5,8 +5,10 @@
 stdenv.mkDerivation rec {
   pname = "cursor-neuro-sama";
   version = "0.0";
-  src =  
-  sourceRoot = ".";
+  src = fetchzip {
+    url = "https://raw.githubusercontent.com/lzc256/nur/Neuro-sama.tar.gz";
+    hash = "sha256-g0s/6rR7gCKWM678EBSLa37OBR+cFJORjFXkiGFqs6U=";
+  };
 
   installPhase = ''
     mkdir -p $out/share/icons
