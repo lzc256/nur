6abc599646c6096987217f68acd17b880ad2e9e4
lzc256 <i@lzc256.com>
2024-10-20 10:42:15 +0800

    Add: lib.lzc256.overrideDrvAsFree

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,13 @@
 { pkgs }:
 
-with pkgs.lib; {
+with pkgs.lib;
+{
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
+
+  lzc256 = {
+    overrideDrvAsFree =
+      pkg: (pkg.overrideAttrs (finalAttrs: previousAttrs: { meta.license = lib.licenses.free; }));
+  };
 }
